af7eab22369cb84771a22ba5b01814c39a523352
Daniel Basta <daniel@basta.info>
2024-02-19 08:28:25 +0100

    put userloader in separate file

---
+++ ynabsplitbudget/userloader.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import yaml
+
+from ynabsplitbudget.client import BaseClient
+from ynabsplitbudget.models.exception import ConfigNotValid, UserNotFound
+from ynabsplitbudget.models.user import User
+
+
+class UserLoader:
+
+	def __init__(self, path: str, user: str):
+		self._path = path
+		self._user_name = user
+		with Path(path).open(mode='r') as f:
+			self._config_dict = yaml.safe_load(f)
+
+		if len(self._config_dict) != 2:
+			raise ConfigNotValid('Config needs to have exactly 2 user entries')
+
+		for u, entries in self._config_dict.items():
+			for e in ['account', 'budget', 'token', 'flag']:
+				if e not in entries.keys():
+					raise ConfigNotValid(f"'{e}' missing for user '{u}'")
+
+	def load(self, user) -> User:
+
+		try:
+			user_dict = self._config_dict[user]
+			c = BaseClient(token=user_dict['token'], user_name=user)
+			account = c.fetch_account(budget_id=user_dict['budget'], account_id=user_dict['account'])
+			return User(name=user, token=user_dict['token'], account=account, flag=user_dict['flag'])
+
+		except KeyError:
+			raise UserNotFound(f"{user} not found in config file at {self._path}")
+
+	@property
+	def user(self) -> User:
+		return self.load(self._user_name)
+
+	@property
+	def partner(self) -> User:
+		partner_name = next(u for u in self._config_dict.keys() if u != self._user_name)
+		return self.load(partner_name)
