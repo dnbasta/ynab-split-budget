86b5081a7a32c9b5c5e3467f014a3950ea7adaf4
Daniel Basta <daniel@basta.info>
2024-02-22 08:03:47 +0100

    added since option to command line

--- ynabsplitbudget/__main__.py
+++ ynabsplitbudget/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import warnings
+from datetime import datetime
 
 from ynabsplitbudget.ynabsplitbudget import YnabSplitBudget
 
@@ -14,7 +15,8 @@ if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 									 usage='ynabsplitaccount [-c | --config] <path/file.yaml#user> '
-										   '[-s | --split-transactions] [-i | --insert-complements]')
+										   '[-s | --split-transactions] [-i | --insert-complements] '
+										   '[-d | --since-date "YYYY-mm-dd"]')
 	parser.add_argument("-c", "--config", type=str, help="path of config YAML to use and user", required=True)
 	parser.add_argument("-s", "--split-transactions", action="store_true",
 						help="split transactions from account")
@@ -22,7 +24,8 @@ if __name__ == '__main__':
 						help="fetch new transactions from both accounts and insert complements")
 	parser.add_argument("-b", "--check-balances", action="store_true",
 						help="raise error if balances of the two accounts don't match")
-
+	parser.add_argument("-d", "--since-date", action="store_true",
+						help='provide date since which insert function should compare transactions in the format of "YYYY-mm-dd"')
 	args = parser.parse_args()
 
 	warnings.showwarning = custom_warn
@@ -32,7 +35,11 @@ if __name__ == '__main__':
 	if args.split_transactions:
 		ysb.split_transactions()
 	if args.insert_complements:
-		ysb.insert_complements()
+		if args.since_date:
+			since = datetime.strptime(args.since_date, "%Y-%m-%d")
+		else:
+			since = None
+		ysb.insert_complements(since=since)
 	if args.check_balances:
 		ysb.raise_on_balances_off()
 
