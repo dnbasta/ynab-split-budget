4487889de3815e893492086e5aea0e8fd15d2da0
Daniel Basta <daniel@basta.info>
2024-05-01 08:40:32 +0200

    renamed clear attribute, changed return value from split to actual transactions

--- ynabsplitbudget/ynabsplitbudget.py
+++ ynabsplitbudget/ynabsplitbudget.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import List, Optional
 
 import yaml
-from ynabtransactionadjuster import Credentials
+from ynabtransactionadjuster import Credentials, Transaction
 
 from ynabsplitbudget.adjusters import ReconcileAdjuster, SplitAdjuster, ClearAdjuster
 from ynabsplitbudget.client import Client
@@ -55,12 +55,12 @@ class YnabSplitBudget:
 		logging.getLogger(__name__).info(f'inserted {len(transactions)} complements into account of {self.partner.name}')
 		return len(transactions)
 
-	def split(self, clear_splits: bool = False) -> int:
+	def split(self, clear: bool = False) -> List[Transaction]:
 		"""Splits transactions (by default 50%) into subtransaction with original category and transfer subtransaction
 		to split account
 
-		:param clear_splits: If set to true transactions in split account will automatically be set to cleared
-		:return: count of split transactions
+		:param clear: If set to true transactions in split account will automatically be set to cleared
+		:return: list with split transactions
 		"""
 		creds = Credentials(token=self.user.token, budget=self.user.budget_id)
 		s = SplitAdjuster(creds, flag_color=self.user.flag_color, transfer_payee_id=self.user.fetch_account().transfer_payee_id,
@@ -69,7 +69,7 @@ class YnabSplitBudget:
 		updated_transactions = s.update(mod_trans)
 		logging.getLogger(__name__).info(f'split {len(updated_transactions)} transactions for {self.user.name}')
 
-		if clear_splits:
+		if clear:
 			transfer_transaction_ids = [st.transfer_transaction_id for t in updated_transactions for st in
 										t.subtransactions if st.transfer_transaction_id]
 			creds = Credentials(token=self.user.token, budget=self.user.budget_id,
@@ -79,7 +79,7 @@ class YnabSplitBudget:
 			count_clear = len(s.update(mod_trans_clear))
 			logging.getLogger(__name__).info(f'cleared {count_clear} transactions for {self.user.name}')
 
-		return len(updated_transactions)
+		return updated_transactions
 
 	def raise_on_balances_off(self):
 		"""Evaluates cleared balances in both accounts
@@ -101,7 +101,8 @@ class YnabSplitBudget:
 		"""
 		since = self._substitute_default_since(since)
 		orphaned_complements = SyncRepository(user=self.user, partner=self.partner).find_orphaned_partner_complements(since)
-		c = Client(self.partner)
+		c = Client(user_name=self.partner.name, budget_id=self.partner.budget_id, account_id=self.partner.account_id,
+				   token=self.partner.token)
 		[c.delete_complement(oc.id) for oc in orphaned_complements]
 		logging.getLogger(__name__).info(f'deleted {len(orphaned_complements)} orphaned complements in account of {self.partner.name}')
 		logging.getLogger(__name__).info(orphaned_complements)
