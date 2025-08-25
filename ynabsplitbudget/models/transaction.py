92a0bab0647cfd0510e2279260e087b14469849e
Daniel Basta <daniel@basta.info>
2025-08-25 10:11:47 +0200

    added insert transaction class

--- ynabsplitbudget/models/transaction.py
+++ ynabsplitbudget/models/transaction.py
@@ -5,29 +5,40 @@ from typing import List
 
 @dataclass
 class RootTransaction:
-	id: str
-	share_id: str
-	transaction_date: date
-	memo: str
-	payee_name: str
-	amount: float
-	account_id: str
+    id: str
+    share_id: str
+    transaction_date: date
+    memo: str
+    payee_name: str
+    amount: float
+    account_id: str
 
 
 @dataclass
 class ComplementTransaction:
-	id: str
-	share_id: str
-	transaction_date: date
-	memo: str
-	payee_name: str
-	amount: float
-	account_id: str
-	iteration: int
+    id: str
+    share_id: str
+    transaction_date: date
+    memo: str
+    payee_name: str
+    amount: float
+    account_id: str
+    iteration: int
 
 
 @dataclass
 class LookupTransaction:
-	account_id: str
-	payee_name: str
-	transfer_transaction_ids: List[str]
+    account_id: str
+    payee_name: str
+    transfer_transaction_ids: List[str]
+
+@dataclass
+class InsertTransaction:
+    id: str
+    share_id: str
+    transaction_date: date
+    memo: str
+    payee_name: str
+    amount: float
+    account_id: str
+    iteration: int
